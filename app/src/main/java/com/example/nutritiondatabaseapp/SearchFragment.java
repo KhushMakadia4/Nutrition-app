afb0db4ec10deba868e0724379cccd1b769b1f07
KhushMakadia4 <kmakadia4@gmail.com>
2021-02-02 00:52:03 -0600

    atleast we have something

---
+++ app/src/main/java/com/example/nutritiondatabaseapp/SearchFragment.java
@@ -0,0 +1,26 @@
+package com.example.nutritiondatabaseapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SearchFragment} factory method to
+ * create an instance of this fragment.
+ */
+public class SearchFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+}
