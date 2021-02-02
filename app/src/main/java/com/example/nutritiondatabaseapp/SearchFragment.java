ef64376289f96f7820f0ac1e836cae80159bde59
KhushMakadia4 <kmakadia4@gmail.com>
2021-02-02 01:31:37 -0600

    atleast we have something

--- app/src/main/java/com/example/nutritiondatabaseapp/SearchFragment.java
+++ app/src/main/java/com/example/nutritiondatabaseapp/SearchFragment.java
@@ -1,7 +1,9 @@
 package com.example.nutritiondatabaseapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,14 +15,12 @@ import android.view.ViewGroup;
  * Use the {@link SearchFragment} factory method to
  * create an instance of this fragment.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
 
-        }
     }
 
 }
